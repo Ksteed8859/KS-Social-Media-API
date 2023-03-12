809fdd99e1b9b634835ca561ff6512c229a56ee9
Kate Steed <sunfiresteed@yahoo.com>
2023-03-12 14:09:57 -0600

    added routes folder and userRoutes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const {
+  getAllUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  updateUser,
+  addFriend,
+  deleteFriend,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+// /api/users/:userId
+router.route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// /api/users/:userId/friends/:friendsId
+router.route('/:userId/friends/:friendsId')
+    .post(addFriend)
+    .delete(deleteFriend);
+
+module.exports = router;
