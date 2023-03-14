645c7f913cd92bf00fafb6a374747210fa820d6d
Kate Steed <sunfiresteed@yahoo.com>
2023-03-14 14:32:38 -0600

    fixed issue with Thought.js when searching user by id

--- models/thought.js
+++ models/thought.js
@@ -1,38 +1,35 @@
-const reactionSchema = require('./Reaction')
 const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
 
-// Schema to create Thought model
 const thoughtSchema = new Schema(
-  {
-    thoughtText: {
-      type: String,
-      required: [true, 'This field cannot be empty'],
-      max_length: [280, 'Max of 280 characters. Delete some characters and try again.'],
-      min_length: [1, 'This field cannot be empty']
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: String,
+            default: Date.now
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
     },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    
-    },
-    username: {
-      type: String,
-      required: true
-    },
-    reactions: [reactionSchema]
-  },
-  {
-    toJSON: {
-      virtuals: true,
-    },
-    id: false
-  }
-);
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+)
 
 thoughtSchema.virtual('reactionCount').get(function () {
-  return this.reactions.length;
+    return this.reactions.length;
 });
 
-const Thought = model('thought', thoughtSchema);
-
-module.exports= Thought;
+const Thought = model('Thought', thoughtSchema);
+module.exports = Thought;
